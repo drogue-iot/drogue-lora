60ffde3a6ba86c1c820e3151a5d5fa8641232d76
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-04-06 08:48:58 +0200

    Add async APIs for lora drivers

---
+++ src/api.rs
@@ -0,0 +1,49 @@
+use crate::types::*;
+use core::future::Future;
+
+/// API for accessing LoRa modules
+pub trait LoraDriver {
+    type ConfigureFuture<'a>: Future<Output = Result<(), LoraError>>
+    where
+        Self: 'a;
+    /// Configure the LoRa module with the provided config.
+    fn configure<'a>(&self, config: &'a LoraConfig) -> Self::ConfigureFuture<'a>;
+
+    type ResetFuture: Future<Output = Result<(), LoraError>>;
+    /// Reset the LoRa module.
+    fn reset(&self, mode: ResetMode) -> Self::ResetFuture;
+
+    type JoinFuture: Future<Output = Result<(), LoraError>>;
+    /// Join a LoRaWAN network with the given connect mode.
+    fn join(&self, mode: ConnectMode) -> Self::JoinFuture;
+
+    type SendFuture<'a>: Future<Output = Result<(), LoraError>>
+    where
+        Self: 'a;
+    /// Send data on a specific port with a given quality of service.
+    fn send<'a>(&self, qos: QoS, port: Port, data: &'a [u8]) -> Self::SendFuture<'a>;
+
+    type SendRecvFuture<'a>: Future<Output = Result<usize, LoraError>>
+    where
+        Self: 'a;
+    /// Send data on a specific port with a given quality of service. If the LoRa module receives
+    /// any data as part of the confirmation, write it into the provided buffer and return the size of the data read.
+    fn send_recv<'a>(
+        &self,
+        qos: QoS,
+        port: Port,
+        data: &'a [u8],
+        rx: &'a mut [u8],
+    ) -> Self::SendRecvFuture<'a>;
+}
+
+#[derive(Debug)]
+pub enum LoraError {
+    SendError,
+    RecvError,
+    RecvTimeout,
+    RecvBufferTooSmall,
+    NotInitialized,
+    NotImplemented,
+    OtherError,
+}
